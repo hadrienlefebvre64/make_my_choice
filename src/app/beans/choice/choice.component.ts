5cbf09d1c491947b630b8e13a28f43645d48a3e0
Hadrien <hadrien.lefebvre@hotmail.fr>
2018-12-10 10:32:29 +0100

    first commit

---
+++ src/app/beans/choice/choice.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choice',
+  templateUrl: './choice.component.html',
+  styleUrls: ['./choice.component.css']
+})
+export class ChoiceComponent {
+
+  id: number;
+  description: string;
+  coefficient: number;
+
+}
