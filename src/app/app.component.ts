c3aca9082b5d89389bd83c288be1a22701d8b04f
Hadrien <hadrien.lefebvre@hotmail.fr>
2018-12-10 12:21:06 +0100

    multi choice algo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,119 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ChoiceComponent} from './beans/choice/choice.component';
+import {MatSlideToggleChange} from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  choices: Array<ChoiceComponent> = [];
+  nbChoice: number;
+  showCoef: boolean;
+  finalChoice : string;
+  showChoice : boolean;
+
+  @Input()
+  choice: ChoiceComponent;
+
+  @Output()
+  change: EventEmitter<MatSlideToggleChange>;
+
+  ngOnInit(): void {
+    this.choices = [];
+    this.nbChoice = 0;
+    this.nbChoice++;
+    let firstChoice = new ChoiceComponent;
+    firstChoice.id = this.nbChoice;
+    this.choices[this.nbChoice - 1] = firstChoice;
+    this.showChoice = false;
+  }
+
+  onSlideChange(ob: MatSlideToggleChange) {
+    ob.checked ? this.showCoef = true : this.showCoef = false;
+
+    if (!this.showCoef){
+      for( let h = 0 ; h < this.choices.length ; h++) {
+        this.choices[h].coefficient = null;
+      }
+    }
+  }
+
+  onAddChoice(choice: ChoiceComponent) {
+
+    this.nbChoice++;
+    let newChoice = new ChoiceComponent;
+    newChoice.id = this.nbChoice;
+    this.choices[this.nbChoice - 1] = newChoice;
+
+  }
+
+  onSupressChoice(id) {
+    console.log(id);
+    console.log(this.choices);
+    // var realId = id - 1;
+    this.choices.splice(this.choices.indexOf(id), 1);
+    this.nbChoice--;
+  }
+
+  onCalculate() {
+
+    let sumCoef = 0;
+    let coefList: Array<number> = [];
+    // Sum of the coeffs
+    for (let j = 0 ; j < this.choices.length ; j++) {
+      this.choices[j].coefficient = +this.choices[j].coefficient;
+      sumCoef = sumCoef + this.choices[j].coefficient;
+    }
+
+    if (!isNaN(sumCoef) && sumCoef != 0) {
+      // We are on a coefficiented list
+      // First we set all coef at list at 1
+      for( let h = 0 ; h < this.choices.length ; h++) {
+        if(isNaN(this.choices[h].coefficient)) {
+          this.choices[h].coefficient = 1;
+        }
+      }
+
+      let resultListSize: number = 0;
+
+      for (let k = 0 ; k < this.choices.length ; k++) {
+        if (!isNaN(this.choices[k].coefficient) && this.choices[k].coefficient != null) {
+          for (let l = 0 ; l < this.choices[k].coefficient ; l++) {
+            coefList[resultListSize] = this.choices[k].id;
+            resultListSize ++ ;
+          }
+        }
+      }
+      // Random on coefList
+      let result = Math.ceil(Math.random() * (coefList.length));
+      let resultId = coefList[result - 1];
+      this.finalChoice = this.choices[resultId - 1].description;
+      this.showChoice = true;
+
+    } else {
+      // We are on a non coefficiented list
+      let result = Math.ceil(Math.random() * (this.choices.length));
+      this.finalChoice = this.choices[result - 1].description;
+      if(this.choices[result - 1].description != "" && this.choices[result - 1].description != undefined){
+        this.showChoice = true;
+      }
+
+    }
+
+  }
+
+  onReset() {
+    this.choices = [];
+    this.nbChoice = 0;
+    this.nbChoice++;
+    let firstChoice = new ChoiceComponent;
+    firstChoice.id = this.nbChoice;
+    this.choices[this.nbChoice - 1] = firstChoice;
+    this.finalChoice = "";
+    this.showChoice = false;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatSelectModule, MatOptionModule, MatInputModule, MatSlideToggleModule, MatToolbarModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import {ChoiceComponent} from './beans/choice/choice.component';
+import {FormsModule} from '@angular/forms';
+import {ResultsComponent} from './results/results.component';
+
+//import {RouterModule, Routes} from '@angular/router';
+
+// const appRoutes: Routes = [
+//   { path : 'results', component: ResultsComponent }
+// ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChoiceComponent,
+    ResultsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    FormsModule,
+    MatToolbarModule
+    // RouterModule.forRoot(
+    //   appRoutes
+    // )
   ],
   providers: [],
   bootstrap: [AppComponent]
